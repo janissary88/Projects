c8b840105b4fe11b3bb5dd14ab508bf2643e3377
Janissary88 <60374600+janissary88@users.noreply.github.com>
2023-11-08 14:48:16 -0500

    first commit

---
+++ CPP practice/hello_world.cpp
@@ -0,0 +1,7 @@
+#include "std_lib_facilities.h"
+int main()  //C++ programs start by executing the function main
+{
+    cout<< "Hello, programming!\nHere we go!\n";    //added new line and comment
+    keep_window_open();
+    return 0;
+}
